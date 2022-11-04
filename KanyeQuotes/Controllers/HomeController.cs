39fcef7d3a1cb7ce98f55ae3935235e0c00d8902
Clément Cadieux <cadieuxclement@gmail.com>
2022-11-03 22:08:43 -0400

    initial

---
+++ KanyeQuotes/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KanyeQuotes.Controllers
+{
+    public class HomeController : Controller
+    {
+        static HttpClient client = new HttpClient();
+        static string quote = null;
+        public async Task<ActionResult> Index()
+        {
+            await GetProductAsync();
+            ViewBag.Quote = quote;
+            return View();
+        }
+
+        static async Task<string> GetProductAsync()
+        {
+            quote = null;
+            HttpResponseMessage response = await client.GetAsync("https://api.kanye.rest");
+            if (response.IsSuccessStatusCode)
+            {
+                quote = await response.Content.ReadAsStringAsync();
+                quote = quote.Substring(10, quote.Length - 12);
+            }
+            return quote;
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
